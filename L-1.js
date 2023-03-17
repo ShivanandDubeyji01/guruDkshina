9b559acd0b8811af9b2b5171e66f65e765a70ebb
ShivanandDubeyji01 <89722709+ShivanandDubeyji01@users.noreply.github.com>
2023-03-17 22:05:35 +0530

    Create

---
+++ L-1.js
@@ -0,0 +1,49 @@
+var a;
+undefined
+ty
+VM66:1 Uncaught ReferenceError: ty is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM66:1
+typeof a;
+'undefined'
+a=10;
+10
+typeof a;
+'number'
+a="baby";
+'baby'
+typeof a;
+'string'
+a=10.1;
+10.1
+typeof a;
+'number'
+a="b";
+'b'
+typeof a;
+'string'
+a=true
+true
+typeof a;
+'boolean'
+var q=900998830389303838n;
+undefined
+typeof q;
+'bigint'
+a+10;
+11
+true+true
+2
+true-true
+0
+var arr=[10,20,30,40,50];
+undefined
+typeof arr;
+'object'
+arr
+(5) [10, 20, 30, 40, 50]0: 101: 202: 303: 404: 50length: 5[[Prototype]]: Array(0)at: ƒ at()concat: ƒ concat()constructor: ƒ Array()copyWithin: ƒ copyWithin()entries: ƒ entries()every: ƒ every()fill: ƒ fill()filter: ƒ filter()find: ƒ find()findIndex: ƒ findIndex()findLast: ƒ findLast()findLastIndex: ƒ findLastIndex()flat: ƒ flat()flatMap: ƒ flatMap()forEach: ƒ forEach()includes: ƒ includes()indexOf: ƒ indexOf()join: ƒ join()keys: ƒ keys()lastIndexOf: ƒ lastIndexOf()length: 0map: ƒ map()pop: ƒ pop()push: ƒ push()reduce: ƒ reduce()reduceRight: ƒ reduceRight()reverse: ƒ reverse()shift: ƒ shift()slice: ƒ slice()some: ƒ some()sort: ƒ sort()splice: ƒ splice()toLocaleString: ƒ toLocaleString()toReversed: ƒ toReversed()toSorted: ƒ toSorted()toSpliced: ƒ toSpliced()toString: ƒ toString()unshift: ƒ unshift()values: ƒ values()with: ƒ with()Symbol(Symbol.iterator): ƒ values()Symbol(Symbol.unscopables): {at: true, copyWithin: true, entries: true, fill: true, find: true, …}[[Prototype]]: Object
+arr[0]=50;
+50
+arr
+(5) [50, 20, 30, 40, 50]
+g==arr;
